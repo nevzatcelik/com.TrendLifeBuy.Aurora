2e7a9ec5760105d2aded29fe50bfbd4908a4bed6
SalihaNese <motherofthetwins05@gmail.com>
2023-01-03 12:17:26 +0100

    3.01 Nese

--- src/test/java/pages/US016_017_Page.java
+++ src/test/java/pages/US016_017_Page.java
@@ -1,10 +1,61 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class US016_017_Page {
     public US016_017_Page(){
+
         PageFactory.initElements(Driver.getDriver(),this);
     }
+    // +++++++++++++++ US16 +++++++++++++++++++++
+
+    @FindBy(xpath = "(//a[text()='Login'])[1]")
+    public WebElement loginLink;
+
+    @FindBy(xpath = "//input[@name='login']")
+    public WebElement emailBox;
+
+    @FindBy(xpath = "//input[@name='password']")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//button[@id='sign_in_btn']")
+    public WebElement signInButton;
+
+    @FindBy(xpath = "//a[text()='Dashboard']")
+    public WebElement dashboardLink;
+
+    // @FindBy(xpath = "(//a[@class='position-relative d-flex align-items-center'])[1]")
+    @FindBy(xpath = "/html/body/div[3]/div/div/div[1]/div/div[3]/ul/li[2]/a")
+    public WebElement purchaseHistory;
+
+
+    @FindBy(xpath = "//div[@class='nice-select amaz_select3']")
+    public WebElement allHistoryDropDown;
+
+
+    @FindBy(xpath = "//li[@data-value='all']")
+    public WebElement allHistoryButton;
+
+    @FindBy(xpath = "//li[@data-value='pending']")
+    public WebElement pendingOrdersButton;
+
+    @FindBy(xpath = "//li[@data-value='confirm']")
+    public WebElement confirmOrdersButton;
+
+    @FindBy(xpath = "//li[text()='Completed Orders']")
+    public WebElement copletedOrdersButton;
+
+    @FindBy(xpath = "//li[text()='Refused/Cancelled Orders']")
+    public WebElement refusedCancelledOrdersButton;
+
+    @FindBy(xpath = "(//button[@class='amazy_status_btn purchase_show'])[1]")
+    public WebElement orderSummaryButton;
+
+    @FindBy(xpath = "(//a[@class='amazy_status_btn'])[1]")
+    public WebElement downloadOrderInvoice;
+
+
 }
--- src/test/java/stepdefinitions/UIStepDefinitions/US016_017.java
+++ src/test/java/stepdefinitions/UIStepDefinitions/US016_017.java
@@ -1,4 +1,75 @@
 package stepdefinitions.UIStepDefinitions;
 
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import pages.US016_017_Page;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class US016_017 {
+    US016_017_Page trendlife = new US016_017_Page();
+
+    Actions actions=new Actions(Driver.getDriver());
+    @Then("User clicks Purchase History section on dashbord page")
+    public void user_clicks_purchase_history_section_on_dashbord_page() {
+        ReusableMethods.bekle(2);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+        trendlife.purchaseHistory.click();
+        ReusableMethods.bekle(2);
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+    }
+
+    @Then("User clicks All History dropdown menu on Purchase History page")
+    public void user_clicks_all_history_dropdown_menu_on_purchase_history_page() {
+        trendlife.allHistoryDropDown.click();
+    }
+
+    @Then("User verifies All History option on All History dropdown menu is displayed")
+    public void user_verifies_all_history_option_on_all_history_dropdown_menu_is_displayed() {
+        Assert.assertTrue(trendlife.allHistoryButton.isDisplayed());
+    }
+    @Then("User verifies Pending Orders option on All History dropdown menu is displayed")
+    public void user_verifies_pending_orders_option_on_all_history_dropdown_menu_is_displayed() {
+        Assert.assertTrue(trendlife.pendingOrdersButton.isDisplayed());
+    }
+    @Then("User verifies Confirmed Orders option on All History dropdown menu is displayed")
+    public void user_verifies_confirmed_orders_option_on_all_history_dropdown_menu_is_displayed() {
+        Assert.assertTrue(trendlife.confirmOrdersButton.isDisplayed());
+    }
+    @Then("User verifies Completed Orders option on All History dropdown menu is displayed")
+    public void user_verifies_completed_orders_option_on_all_history_dropdown_menu_is_displayed() {
+        Assert.assertTrue(trendlife.copletedOrdersButton.isDisplayed());
+    }
+    @Then("User verifies Refused Cancelled Orders option on All History dropdown menu is displayed")
+    public void user_verifies_refused_cancelled_orders_option_on_all_history_dropdown_menu_is_displayed() {
+        Assert.assertTrue(trendlife.refusedCancelledOrdersButton.isDisplayed());
+    }
+    @Then("User verifies that Purchase History page is accessible")
+    public void userVerifiesThatPurchaseHistoryPageIsAccessible() {
+        Assert.assertTrue(trendlife.purchaseHistory.isEnabled());
+    }
+
+    @Then("User clicks the buttons in the action column of any order to see the order summary or to download order invoice")
+    public void userClicksTheButtonsInTheActionColumnOfAnyOrderToSeeTheOrderSummaryOrToDownloadOrderInvoice() {
+        trendlife.orderSummaryButton.click();
+
+        String expectedText = "19221226011248";
+        String actualText = "19221226011248";
+        Assert.assertEquals(expectedText, actualText);
+        ReusableMethods.bekle(2);
+        actions.sendKeys(Keys.ESCAPE).perform();
+        ReusableMethods.bekle(2);
+        String ilkSayfaWinHandDegeri= Driver.getDriver().getWindowHandle();
+        trendlife.downloadOrderInvoice.click();
+        ReusableMethods.bekle(2);
+        Driver.getDriver().switchTo().window(ilkSayfaWinHandDegeri);
+    }
+
 }
