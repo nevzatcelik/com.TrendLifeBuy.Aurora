37938bc3931a50a7cfba13a6e6e6d15183db31c8
Fatih F YILDIZ <fatihfyildiz@gmail.com>
2023-01-04 17:17:37 +0100

    04.01(1) Fatih

--- src/test/java/runners/RegressionTestRunner.java
+++ src/test/java/runners/RegressionTestRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         },
         features ="src/test/resources/features",
         glue={"stepdefinitions", "hooks"},
-        tags= "@regression",
+        tags= "@fatih",
         dryRun =false
 )
 public class RegressionTestRunner {
