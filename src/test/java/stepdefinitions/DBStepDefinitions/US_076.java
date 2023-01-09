2c78f9bcc012ec45ced6eee55f78f07c6f46f293
ZynpZ <zetok7834@gmail.com>
2023-01-10 02:06:10 +0300

    10.01(1)

---
+++ src/test/java/stepdefinitions/DBStepDefinitions/US_076.java
@@ -0,0 +1,18 @@
+package stepdefinitions.DBStepDefinitions;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class US_076 {
+
+    String url = "jdbc:mysql://45.84.205.255:3306/u480337000_tlb_training";
+    String username = "u480337000_tbl_training_u";
+    String password = "O+e3!xmZcO]";
+
+    Connection connection;
+    Statement statement;
+    ResultSet resultSet;
+
+
+}
---
+++ src/test/resources/dataBasefeatures/[US0076] ADD PRODUCT TO CART.feature
@@ -0,0 +1,6 @@
+
+
+Feature: US0076 As an administrator, I should be able to add products to the cart via DataBase,
+  and verify that the added product is in the Carts list.
+
+  Scenario: TC_7601 I should be able to add products to the Carts table
